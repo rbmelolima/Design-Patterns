3398155b849069d34bc1debe4581c5e22c610eee
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2022-02-12 13:35:26 -0300

    refactor: adicionando comentários no observer

--- behavioral_patterns/observer.dart
+++ behavioral_patterns/observer.dart
@@ -1,7 +1,13 @@
+/**
+  A interface do Assinante declara a interface de notificação. Na maioria dos casos ela consiste de um único método atualizar. O método pode ter vários parâmetros que permite que a publicadora passe alguns detalhes do evento junto com a atualização.
+ */
 abstract class Subscriber {
   void update() => throw Exception();
 }
 
+/*
+  A Publicadora manda eventos de interesse para outros objetos. Esses eventos ocorrem quando a publicadora muda seu estado ou executa algum comportamento. As publicadoras contêm uma infraestrutura de inscrição que permite novos assinantes se juntar aos atuais assinantes ou deixar a lista.
+*/
 class Publisher {
   List<Subscriber> _subscribers = [];
 
